2fc03600fe574963caf2ddf72e6c813abc0d5051
rtoledo_dev <rodrigo@rtoledo.inf.br>
2024-04-02 10:01:56 -0300

    Create tasks migration, schema, seeds, and directories

---
+++ db/migrate/20240402021150_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.datetime :scheduled_at
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+5.times.each do
+  Task.create(title: Faker::Lorem.sentence(word_count: 4), description: Faker::Lorem.paragraph)
+end
