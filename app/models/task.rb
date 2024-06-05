9963df99625d6c09d1f14cc4680efcc09cfe78f6
rtoledo_dev <rodrigo@rtoledo.inf.br>
2024-06-05 12:13:11 -0300

    Update Stimulus Reflex gem version to 3.5

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,13 +1,10 @@
+# frozen_string_literal: true
+
 class Task < ApplicationRecord
+  validates :title, presence: true
+
   after_create_commit do
-    broadcast_prepend_to 'tasks', partial: 'tasks/task', locals: { task: self }
+    broadcast_prepend_to 'tasks', target: 'tasks', partial: 'tasks/task', locals: { task: self }
   end
-
-  after_update_commit do
-    broadcast_replace_to 'tasks', target: "task_#{id}", partial: 'tasks/task', locals: { task: self }
-  end
-
-  after_destroy do
-    broadcast_remove_to 'tasks', target: "task_#{id}"
-  end
+  broadcasts_refreshes
 end
