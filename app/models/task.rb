a9f9558c7d0b754062dbcfa4b8ebfa4c889c73d3
Rodrigo Toledo <rodrigotoledo@Rodrigos-MacBook-Pro.local>
2024-10-13 16:12:05 -0300

    Update task broadcasting methods for create, update, delete

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,14 @@ class Task < ApplicationRecord
   validates :title, presence: true
 
   after_create_commit do
-    broadcast_prepend_to 'tasks', target: 'tasks', partial: 'tasks/task', locals: { task: self }
+    broadcast_prepend_to 'tasks', partial: 'tasks/task', locals: { task: self }
   end
-  broadcasts_refreshes
+
+  after_update_commit do
+    broadcast_replace_to 'tasks', target: "task_#{id}", partial: 'tasks/task', locals: { task: self }
+  end
+
+  after_destroy do
+    broadcast_remove_to 'tasks', target: "task_#{id}"
+  end
 end
