28b199c17e7f16c0f81222076dbd563cf139ee12
rtoledo dev <rodrigo@rtoledo.inf.br.>
2025-07-02 21:51:33 -0300

    Refactor configuration for Solide configurations
    - Replace default cache and queue adapters with solid alternatives.
    - Introduce schemas for cable, cache, and queue management.
    - Update environment configurations for development, test, and production.
    - Add `frozen_string_literal: true` pragma to all files.
    - Set up letter_opener in development for mail previews.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_021150) do
+ActiveRecord::Schema[8.0].define(version: 2024_04_02_021150) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -19,5 +19,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_02_021150) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
